9ec1ce6e26519479afcf92f3372e9a94f9d614b5
aidagalimova <aidagalimova03@gmail.com>
2024-07-08 15:17:07 +0300

    feat: исправила хук useMediaQuery

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -2,28 +2,20 @@ import { useEffect, useState } from "react";
 import { useMediaQueryType } from "../types";
 
 export const useMediaQuery = (props: useMediaQueryType) => {
-  const mediaQueriesList = Object.values(props).map((query) =>
-    window.matchMedia(query)
-  );
+  const mediaQueries = Object.values(props).join(" and ");
 
   const [mediaQueryMatches, setMediaQueryMatches] = useState(
-    mediaQueriesList.every((mediaQuery) => mediaQuery.matches)
+    window.matchMedia(mediaQueries).matches
   );
 
-  const handleChange = () => {
-    setMediaQueryMatches(
-      mediaQueriesList.every((mediaQuery) => mediaQuery.matches)
-    );
+  const handleChange = (e: MediaQueryListEvent) => {
+    setMediaQueryMatches(e.matches);
   };
 
   useEffect(() => {
-    mediaQueriesList.forEach((mediaQueryList) =>
-      mediaQueryList.addEventListener("change", handleChange)
-    );
-    return () =>
-      mediaQueriesList.forEach((mediaQueryList) =>
-        mediaQueryList.removeEventListener("change", handleChange)
-      );
+    const mediaQueryList = window.matchMedia(mediaQueries);
+    mediaQueryList.addEventListener("change", handleChange);
+    return () => mediaQueryList.removeEventListener("change", handleChange);
   }, []);
 
   return mediaQueryMatches;
